ff5a92c7f4f29f4e458429095e60b454f1e88996
Matías Pnoexz Pino <pnoexz+gh@gmail.com>
2018-01-13 14:25:29 +0000

    Added quick filter button next to subreddits name. Probably added a bunch of bugs too.

--- PRALL.user.js
+++ PRALL.user.js
@@ -4,56 +4,65 @@
 // @include     https://www.reddit.com/r/all/*
 // @version     1
 // @grant       none
+// @require     https://code.jquery.com/jquery-3.2.1.min.js
 // ==/UserScript==
 
 console = unsafeWindow.console
-var rules;
+var rules = [];
+var multis = [];
+var me = [];
+
+$.ajax({
+  type: 'GET',
+  url: 'https://www.reddit.com/api/me.json',
+  success: function(data) {
+    me = data;
+  },
+  error: function(XMLHttpRequest, textStatus, errorThrown) {
+    console.debug(XMLHttpRequest);
+    alert("Error getting user info: " + errorThrown); 
+  }
+})
+
+
 
 function ApiGet() {
-$.ajax({
-	url: 'https://www.reddit.com/api/multi/mine',
-	success: function(data) {
-		console.log('Fetched multi');
-		// console.dir(data);
-		return ApiParse(data);
-	}
-})
+  $.ajax({
+    url: 'https://www.reddit.com/api/multi/mine',
+    success: function(data) {
+      console.log('Fetched multi');
+      // console.dir(data);
+      return ApiParse(data);
+    }
+  })
 }
 
 function ApiParse(data) {
-	//data = [{"kind": "LabeledMulti", "data": {"can_edit": true, "display_name": "prall", "name": "prall", "description_html": "", "created": 1460065506.0, "copied_from": "/user/pnoexz/m/shitholes", "icon_url": null, "subreddits": [{"name": "TedCruzForPresident"}, {"name": "politics"}, {"name": "JebBush"}, {"name": "2016_elections"}, {"name": "news"}, {"name": "hillaryclinton"}, {"name": "trump"}, {"name": "donaldtrump"}, {"name": "nba"}, {"name": "sports"}, {"name": "Drumpf"}, {"name": "The_Donald"}, {"name": "TrumpForPresident"}, {"name": "Donald_Trump"}, {"name": "SandersForPresident"}, {"name": "soccer"}, {"name": "BlackPeopleTwitter"}], "created_utc": 1460036706.0, "key_color": "#cee3f8", "visibility": "private", "icon_name": "", "weighting_scheme": "classic", "path": "/user/Pnoexz/m/prall", "description_md": ""}}, {"kind": "LabeledMulti", "data": {"can_edit": true, "display_name": "prall_usa", "name": "prall_usa", "description_html": "", "created": 1460182685.0, "copied_from": null, "icon_url": null, "subreddits": [{"name": "TedCruzForPresident"}, {"name": "Hillary"}, {"name": "2016_elections"}, {"name": "conservatives"}, {"name": "hillaryclinton"}, {"name": "trump"}, {"name": "donaldtrump"}, {"name": "2016Elections"}, {"name": "Drumpf"}, {"name": "KasichForPresident"}, {"name": "trump16"}, {"name": "BernieSanders"}, {"name": "The_Donald"}, {"name": "TrumpForPresident"}, {"name": "TimCanova"}, {"name": "Donald_Trump"}, {"name": "JebBush"}, {"name": "bernieforpresident"}, {"name": "AskTrumpSupporters"}, {"name": "Marco_Rubio"}], "created_utc": 1460153885.0, "key_color": "#cee3f8", "visibility": "public", "icon_name": "", "weighting_scheme": "classic", "path": "/user/Pnoexz/m/prall_usa", "description_md": ""}}, {"kind": "LabeledMulti", "data": {"can_edit": true, "display_name": "totallysane", "name": "totallysane", "description_html": "", "created": 1460065886.0, "copied_from": null, "icon_url": null, "subreddits": [{"name": "TheMixedNuts"}, {"name": "Schizoid"}], "created_utc": 1460037086.0, "key_color": "#cee3f8", "visibility": "private", "icon_name": "", "weighting_scheme": "classic", "path": "/user/Pnoexz/m/totallysane", "description_md": ""}}];
-
+  multis = data;
 	for (i in data) {
 		if (data[i].kind == 'LabeledMulti') {
 
-			if (data[i].data.display_name == 'prall') {
+			if (data[i].data.display_name == 'prall' || data[i].data.display_name == 'prall2' || data[i].data.description_md == 'PRALL') {
 				var multi = data[i].data;
-				rules = [];
 				for (k in multi.subreddits) {
 					rules.push(multi.subreddits[k].name);
 				}
-				console.log('Parsed data');
-				//console.dir(rules);
-
-				return ApplyFilter();
-
+				console.log('Parsed data from multi: ' + data[i].data.display_name);
 			}
 		}
 	}
-	console.log('No suitable data found');
-	return false;
-
+  console.log('Total rules added: ' + rules.length)
+	return ApplyFilter();
 }
 
 
 function ApplyFilter() {
-	//rules = [ "TedCruzForPresident", "soccer", "AdviceAnimals", "politics", "JebBush", "2016_elections", "news", "hillaryclinton", "trump", "donaldtrump", "nba", "sports" ];
 	if (typeof rules == 'undefined') {
 		console.log('no rules');
 		return false;
 	}
-	console.log('Applying filter');
-	console.dir(rules);
+	console.log('Applying filter for ' + rules.length + ' subreddits');
+	//console.dir(rules);
 	for (j in rules) {
 		$('div[data-subreddit="'+rules[j]+'"]').hide()
 	}
@@ -62,6 +71,80 @@ function ApplyFilter() {
 
 console.log('PRALL loaded.');
 ApiGet();
+
+function confirmFilterSubreddit(name) {
+  if (confirm('Are you sure you want to filter ' + name) || true) {
+    console.log('Filtering for real');
+    filterSubreddit(name);
+  } else {
+  }
+}
+
+function filterSubreddit(subredditName) {
+  var multi = findMultiWithSpace();
+  var multiName = multi.data.name;
+
+  $.ajax({
+    url: 'https://www.reddit.com/api/multi/user/'+me.data.name+'/m/' + multiName + '//r/' + subredditName,
+    type: 'PUT',
+    contentType: "application/x-www-form-urlencoded",
+    data: 'model=%7B%22name%22%3A%22' + multiName + '%22%7D',
+    headers: {
+      'X-modhash': me.data.modhash,
+      'X-Requested-With': 'XMLHttpRequest'
+    },
+    success: function(data) {
+      console.log('Added ' + subredditName + ' to PRALL multi ' + multiName);
+      ApiGet();
+    },
+    error: function(XMLHttpRequest, textStatus, errorThrown) {
+      console.debug(XMLHttpRequest);
+      alert("Error filtering " + subredditName); 
+    }
+  })
+}
+
+function findMultiWithSpace() {
+  for (i in multis) {
+		if (multis[i].kind == 'LabeledMulti' && multis[i].data.description_md == 'PRALL') {
+      if (!isMultiFull(multis[i])) {
+        console.log('Found multi with room: ' + multis[i].data.name);
+        return multis[i];
+      } else {
+        console.log('Rejecting multi: ' + multis[i].data.name);
+      }
+    }
+  }
+  alert('Didnt find any suitable multi. Create a multi and set its description to: "PRALL"');
+}
+
+function isMultiFull(multi) {
+  if (multi.data.subreddits.length > 99) {
+    return true;
+  }
+  return false;
+}
+
+function drawButtons() {
+  console.log('Drawing buttons');
+  $('p.tagline').each(function() {
+    var button = $(document.createElement('a'));
+    button.text('[-]');
+    button.css({
+      'padding-left': '5px',
+      'cursor': 'pointer'
+    });
+    var subreddit = $(this).parent().parent().parent().attr('data-subreddit');
+    button.attr('data-subreddit', subreddit);
+    
+    button.on('click', function () {
+      confirmFilterSubreddit(subreddit);
+    });
+    $(this).append(button);
+  });
+}
+                      
+drawButtons();
 
 
 
@@ -83,10 +166,16 @@ observer.observe(document.getElementById('siteTable'), {
 });
 
 
-/** Button
+/* Button
 var btn = $(document.createElement('button'));
 
 btn.text('start');
 btn.on('click',function() { ApplyFilter() });
 
 $('#header-bottom-right').append(btn);*/
+
+/**
+@TODO add useragent to requests
+@TODO autocreate multi when all are full
+@TODO cleanup
+*/
